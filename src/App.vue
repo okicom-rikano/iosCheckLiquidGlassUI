e061972dff045a73d3300c3e5fd06f4375bee518
okicom-rikano <82216341+okicom-rikano@users.noreply.github.com>
2025-10-15 14:33:05 +0900

    overflow-scrollクラスを追加し、テキストエリアを追加

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ const height = ref('h-dvh');
 </script>
 
 <template>
-  <div class="flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100 border-b-4 border-red-500 p-4" :class="height">
+  <div class="overflow-scroll flex items-start justify-center bg-gradient-to-br from-blue-50 to-indigo-100 border-b-4 border-red-500 p-4" :class="height">
     <div class="text-center">
       <h1 class="text-4xl font-bold text-gray-800 mb-8">
         Vue 3 + Vite + TypeScript + Tailwind
@@ -21,6 +21,12 @@ const height = ref('h-dvh');
         <button type="button" class="bg-blue-700 text-white px-4 py-2 font-xs rounded-md" @click="height = 'h-lvh'">h-lvh(100lvh)</button>
       </div>
       <ModalExample />
+
+      <div>
+        <textarea id="story" name="story" rows="20" class="mt-4 w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="ここにテキストを入力してください...">
+          親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。別段深い理由でもない。新築の二階から首を出していたら、同級生の一人が冗談に、いくら威張っても、そこから飛び降りる事は出来まい。弱虫やーい。と囃したからである。小使に負ぶさって帰って来た時、おやじが大きな眼をして二階ぐらいから飛び降りて腰を抜かす奴があるかと云ったから、この次は抜かさずに飛んで見せますと答えた。（青空文庫より）
+        </textarea>
+      </div>
     </div>
   </div>
 </template>
